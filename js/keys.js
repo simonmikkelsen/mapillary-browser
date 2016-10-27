f8f8c175f179d37f58c1e6491b2d6060558a984b
smn <simon.mikkelsen@systematic.com>
2016-10-27 12:23:20 +0200

    Added header and comments about MApbox key.

--- js/keys.js
+++ js/keys.js
@@ -1,4 +1,5 @@
  // Log into mapillary.com, go to settings, applications, create an application and use the public ID here:
-var clientId = "Y0NtM3R4Zm52cTBOSUlrTFAwWFFFQTo5OWYyZGMzYjY4ZGU3ZGZh";
+var clientId = "";
 
-var mapboxPk = "pk.eyJ1IjoidHJ5bCIsImEiOiJjaXVqb3gyY3owMDBrMnRwOXptb3NyZGRjIn0.xjtavWU7nPwvkBErlfrAZw";
+// Your MapBox key.
+var mapboxPk = "";
