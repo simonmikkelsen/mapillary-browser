fe1c869b804bf817843bc2ad50f6dd27f25ca2a5
smn <simon.mikkelsen@systematic.com>
2017-01-26 14:14:31 +0100

    Added the current database schema.

--- db/mexplorer.sql
+++ db/mexplorer.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Vært: localhost:3306
--- Genereringstid: 09. 12 2016 kl. 14:34:18
+-- Genereringstid: 26. 01 2017 kl. 13:13:59
 -- Serverversion: 5.5.49-log
 -- PHP-version: 7.0.9
 
@@ -39,6 +39,35 @@ CREATE TABLE IF NOT EXISTS `image` (
 -- --------------------------------------------------------
 
 --
+-- Struktur-dump for tabellen `image_list`
+--
+
+CREATE TABLE IF NOT EXISTS `image_list` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_bin NOT NULL,
+  `user` int(11) NOT NULL,
+  `public` tinyint(1) NOT NULL,
+  `locked` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur-dump for tabellen `image_list_item`
+--
+
+CREATE TABLE IF NOT EXISTS `image_list_item` (
+  `id` int(11) NOT NULL,
+  `image` int(11) NOT NULL,
+  `text` int(11) DEFAULT NULL,
+  `added` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `image_order` int(11) NOT NULL,
+  `list` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
 -- Struktur-dump for tabellen `tag`
 --
 
@@ -54,19 +83,19 @@ CREATE TABLE IF NOT EXISTS `tag` (
 --
 DELIMITER $$
 CREATE TRIGGER `tag_rev__ai` AFTER INSERT ON `tag`
- FOR EACH ROW INSERT INTO mexplorer.tag_rev SELECT NULL, 'insert', 0, 'username', '127.0.0.1', 'ipv6', 0, NOW(), d.* 
+ FOR EACH ROW INSERT INTO mexplorer.tag_rev SELECT NULL, 'insert', NOW(), d.* 
     FROM mexplorer.tag AS d WHERE d.image = NEW.image AND d.keytext = NEW.keytext
 $$
 DELIMITER ;
 DELIMITER $$
 CREATE TRIGGER `tag_rev__au` AFTER UPDATE ON `tag`
- FOR EACH ROW INSERT INTO mexplorer.tag_rev SELECT NULL, 'update', 0, 'username', '127.0.0.1', 'ipv6', 0, NOW(), d.* 
+ FOR EACH ROW INSERT INTO mexplorer.tag_rev SELECT NULL, 'update', NOW(), d.* 
     FROM mexplorer.tag AS d WHERE d.image = NEW.image AND d.keytext = NEW.keytext
 $$
 DELIMITER ;
 DELIMITER $$
 CREATE TRIGGER `tag_rev__bd` BEFORE DELETE ON `tag`
- FOR EACH ROW INSERT INTO mexplorer.tag_rev SELECT NULL, 'delete', 0, 'username', '127.0.0.1', 'ipv6', 0, NOW(), d.* 
+ FOR EACH ROW INSERT INTO mexplorer.tag_rev SELECT NULL, 'delete', NOW(), d.* 
     FROM mexplorer.tag AS d WHERE d.image = OLD.image AND d.keytext = OLD.keytext
 $$
 DELIMITER ;
@@ -80,11 +109,6 @@ DELIMITER ;
 CREATE TABLE IF NOT EXISTS `tag_rev` (
   `id` int(11) NOT NULL,
   `action` varchar(6) COLLATE utf8_bin NOT NULL,
-  `user` int(11) DEFAULT NULL,
-  `username` varchar(255) COLLATE utf8_bin NOT NULL,
-  `ipv4address` varchar(15) COLLATE utf8_bin DEFAULT NULL,
-  `ipv6address` varchar(45) COLLATE utf8_bin DEFAULT NULL,
-  `port` smallint(5) unsigned NOT NULL,
   `changedate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `mapillary_key` char(32) COLLATE utf8_bin NOT NULL,
   `image` int(11) DEFAULT NULL,
@@ -99,7 +123,7 @@ CREATE TABLE IF NOT EXISTS `tag_rev` (
 --
 
 CREATE TABLE IF NOT EXISTS `user` (
-  `uuid` char(40) COLLATE utf8_bin NOT NULL,
+  `id` int(11) NOT NULL,
   `user` varchar(255) COLLATE utf8_bin NOT NULL,
   `avatar` varchar(255) COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
@@ -116,6 +140,24 @@ ALTER TABLE `image`
   ADD UNIQUE KEY `mapillary_key` (`mapillary_key`),
   ADD KEY `mapillary_key_2` (`mapillary_key`),
   ADD KEY `captured_at` (`captured_at`);
+
+--
+-- Indeks for tabel `image_list`
+--
+ALTER TABLE `image_list`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`,`user`),
+  ADD KEY `user` (`user`),
+  ADD KEY `user_2` (`user`);
+
+--
+-- Indeks for tabel `image_list_item`
+--
+ALTER TABLE `image_list_item`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `image_2` (`image`,`list`),
+  ADD KEY `list` (`list`),
+  ADD KEY `image` (`image`);
 
 --
 -- Indeks for tabel `tag`
@@ -135,8 +177,9 @@ ALTER TABLE `tag_rev`
 -- Indeks for tabel `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`uuid`),
-  ADD KEY `user_index` (`user`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_index` (`user`),
+  ADD KEY `id` (`id`);
 
 --
 -- Brug ikke AUTO_INCREMENT for slettede tabeller
@@ -146,6 +189,16 @@ ALTER TABLE `user`
 -- Tilføj AUTO_INCREMENT i tabel `image`
 --
 ALTER TABLE `image`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Tilføj AUTO_INCREMENT i tabel `image_list`
+--
+ALTER TABLE `image_list`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Tilføj AUTO_INCREMENT i tabel `image_list_item`
+--
+ALTER TABLE `image_list_item`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- Tilføj AUTO_INCREMENT i tabel `tag`
@@ -158,8 +211,26 @@ ALTER TABLE `tag`
 ALTER TABLE `tag_rev`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
+-- Tilføj AUTO_INCREMENT i tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- Begrænsninger for dumpede tabeller
 --
+
+--
+-- Begrænsninger for tabel `image_list`
+--
+ALTER TABLE `image_list`
+  ADD CONSTRAINT `user` FOREIGN KEY (`user`) REFERENCES `user` (`id`) ON DELETE CASCADE;
+
+--
+-- Begrænsninger for tabel `image_list_item`
+--
+ALTER TABLE `image_list_item`
+  ADD CONSTRAINT `image` FOREIGN KEY (`image`) REFERENCES `image` (`id`),
+  ADD CONSTRAINT `list` FOREIGN KEY (`list`) REFERENCES `image_list` (`id`);
 
 --
 -- Begrænsninger for tabel `tag`
